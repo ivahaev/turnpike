2712de7a802ba67f749ad74a0f6f4c094967e5de
ivahaev <ivahaev@gmail.com>
2015-09-08 22:32:18 +0500

    Delete extra

--- turnpike.go
+++ turnpike.go
@@ -5,10 +5,10 @@
 
 // Package turnpike provides a Websocket Application Messaging Protocol (WAMP) server and client
 package turnpike
-
-import (
-    "code.google.com/p/go.net/websocket"
-)
+//
+//import (
+//    "code.google.com/p/gfuncsfuno.net/websocket"
+//)
 
 const (
     turnpikeVersion = "0.2.2"
@@ -16,15 +16,15 @@ const (
 )
 
 var debug = false
-
-// Handler is a interface to support Go1.0.
-type Handler interface {
-    HandleWebsocket(*websocket.Conn)
-}
-
-// HandleWebsocket is a Go1.0 shim for method values.
-func HandleWebsocket(t Handler) func(*websocket.Conn) {
-    return func(conn *websocket.Conn) {
-        t.HandleWebsocket(conn)
-    }
-}
+//
+//// Handler is a interface to support Go1.0.
+//type Handler interface {
+//    HandleWebsocket(*websocket.Conn)
+//}
+//
+//// HandleWebsocket is a Go1.0 shim for method values.
+//func HandleWebsocket(t Handler) func(*websocket.Conn) {
+//    return func(conn *websocket.Conn) {
+//        t.HandleWebsocket(conn)
+//    }
+//}
